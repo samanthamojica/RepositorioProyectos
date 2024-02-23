93c9293c46e5c729eb7bfeb1f5d1ec25bd4d2528
samanthamojica <samanthamojica@hotmail.com>
2024-02-22 18:27:43 -0600

    obtener las categorias que envia el back

--- Venta de Joyeria/app/src/app/models/categorias-anillos.ts
+++ Venta de Joyeria/app/src/app/models/categorias-anillos.ts
@@ -1,4 +1,6 @@
 export class CategoriasAnillos {
+  public nombreCategoria: string;
+  public idCategoria: number;
   
   constructor( idCategoria: number, nombreCategoria:string) {}
 }
--- Venta de Joyeria/app/src/app/servicios/anillos.service.ts
+++ Venta de Joyeria/app/src/app/servicios/anillos.service.ts
@@ -1,32 +1,24 @@
-import { DebugElement, Injectable } from '@angular/core';
+import {  Injectable } from '@angular/core';
 import { Anillo } from '../models/anillo';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CategoriasAnillos } from '../models/categorias-anillos';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnillosService {
   anillo: Anillo;
-  url = 'http://localhost:8080/anillos/';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   saveImagenesEnBucket(file: FormData): Observable<Map<string, string>> {
-    return this.http.post<Map<string, string>>('http://localhost:8080/imagenes/subirImagenes',  file );
+    return this.http.post<Map<string, string>>( 'http://localhost:8080/imagenes/subirImagenes',  file );
   }
 
   saveAnilloBD(anillo: Anillo): Observable<boolean> {
-    console.log("entro al metodo guardar anillo en BD");
-    debugger
-    return this.http.post<boolean>('http://localhost:8080/anillos/anillo/' , anillo);
+       return this.http.post<boolean>( 'http://localhost:8080/anillos/anillo/',   anillo );
   }
-
-obtenerCategoriasAnillos():Observable<CategoriasAnillos>{
-  return this.http.get<CategoriasAnillos>('http://localhost:8080/');
-}
-
 }
