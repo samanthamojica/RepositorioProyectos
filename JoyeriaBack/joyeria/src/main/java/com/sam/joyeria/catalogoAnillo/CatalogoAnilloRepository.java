5cddc68e4a675900307fe147df5bc2ef8dfc8bc0
samanthamojica <samanthamojica@hotmail.com>
2024-03-07 19:34:05 -0600

    falta obtener informacion de anillo por idCategoria, (CHECAR BD)

--- JoyeriaBack/joyeria/src/main/java/com/sam/joyeria/catalogoAnillo/CatalogoAnilloRepository.java
+++ JoyeriaBack/joyeria/src/main/java/com/sam/joyeria/catalogoAnillo/CatalogoAnilloRepository.java
@@ -1,11 +1,13 @@
 package com.sam.joyeria.catalogoAnillo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sam.joyeria.catalogoAnillo.model.CatalogoAnillo;
 
 @Repository
-public interface CatalogoAnilloRepository extends JpaRepository<CatalogoAnillo, Integer> {
-
-}
+public interface CatalogoAnilloRepository extends JpaRepository<CatalogoAnillo, Integer> {}
